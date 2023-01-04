4026e92ecaffc4caf585723e038c954d94eb20c3
seanmorton2023 <63743881+seanmorton2023@users.noreply.github.com>
2023-01-03 22:37:02 -0600

    detection.py and motor_controller.py
    - made some code more modular in detection.py to make integration and debug easier
    - made general structure of initial motor controller functions

--- detection.py
+++ detection.py
@@ -13,8 +13,106 @@ import datetime
 import matplotlib.pyplot as plt
 from collections import OrderedDict
 from centroid_tracker import CentroidTracker
-import matplotlib.pyplot as plt
 from visualize_tracking import visualize
+
+def detect_bboxes(yolo_net, layerNames, confid, tracking=False):
+    '''Carries out the heavy lifting of the object detection model. Takes
+    in the YOLO model, detects potential matches for the "ball", and uses
+    non-maximum suppression (NMS) to remove redundant data.
+
+    Returns: boundingboxes, a list of bounding box objects
+    '''
+
+    # YOLO Detection
+    blob = cv2.dnn.blobFromImage(frame, 1 / 255.0, (416, 416), swapRB=True, crop=False)
+    yolo_net.setInput(blob)
+    layerOutputs = yolo_net.forward(layerNames)
+
+    boxes = []
+    confidences = []
+    classIDs = []
+
+    # loop over each detection
+    for output in layerOutputs:
+        for detection in output:
+            # extract the class ID and confidence of the current object detection
+            scores = detection[5:]
+            classID = np.argmax(scores)
+            confidence = scores[classID]
+
+            # filtering detections to just people with confidence above minimum confidence argument
+            if classID == 0 and confidence > confid:
+                # scale the bounding box coordinates back relative to the size of the image
+                box = detection[0:4] * np.array([W, H, W, H])
+                (centerX, centerY, width, height) = box.astype("int")
+
+                # calculate coordinates of top left point of box
+                x = int(centerX - (width / 2))
+                y = int(centerY - (height / 2))
+
+                boxes.append([x, y, int(width), int(height)])
+                confidences.append(float(confidence))
+                classIDs.append(classID)
+
+    # apply non-maximum suppression algorithm and store final results in bounding box list
+    nms_boxes = cv2.dnn.NMSBoxes(boxes, confidences, confid, threshold)
+    boundingboxes = []
+    for i in range(len(boxes)):
+        if i in nms_boxes:
+            boundingboxes.append(boxes[i])
+
+    return boundingboxes
+
+def process_frame(vs, H, W, ct, yolo_net, layerNames, confid, tracking=False):
+    '''Takes a source of video input, captures + resizes a frame, locates the objects of interest,
+    and carries out centroid tracking.
+
+    Returns: the modified frame, as well as the bounding boxes that have been edited
+    by centroid tracking code
+    '''
+    (grabbed, frame) = vs.read()
+
+    # break if using video input and no frame left to grab
+    if args["input"] is not None and frame is None:
+        print("End of Video File")
+        return None, None
+
+    # resizing frames for consistency, regardless of size of input video
+    frame = imutils.resize(frame, width=1250)
+
+    # grabbing frame dimensions
+    if W is None or H is None:
+        (H, W) = frame.shape[:2]
+
+    boundingboxes = detect_bboxes(yolo_net, layerNames, confid, tracking=tracking)
+    print("first: ", boundingboxes)
+    # call update function in centroid tracker, draw unique ID above bounding box
+
+    if tracking:
+        objects = ct.update(frame, boundingboxes)
+        # turning ordered dictionary into list of tuples
+        boundingboxes = [(ID, box) for ID, box in objects.items()]
+    else:
+        boundingboxes = [(ID, box) for ID, box in enumerate(boundingboxes)]
+
+    print("second: ", boundingboxes)
+    return frame, boundingboxes
+
+def construct_args():
+    # constructing argument parser and parsing arguments
+    ap = argparse.ArgumentParser()
+    ap.add_argument("-i", "--input", type=str,
+                    help="path to input video")
+    ap.add_argument("-c", "--confidence", type=float, default=0.5,
+                    help="minimum probability to filter weak detections")
+    ap.add_argument("-t", "--threshold", type=float, default=0.3,
+                    help="threshold when applying non-maximum suppression")
+    ap.add_argument("-d", "--disappeared", type=int, default=10,
+                    help="number of frames that an object ID is not detected before it is de-registered")
+    ap.add_argument("-tr", "--tracking", type=int, default=False,
+                    help="number of frames between data output to text file")
+    args = vars(ap.parse_args())
+    return args
 
 # TODO: Cythonize this
 def detection_and_tracking(vid_input, yolo_net, layerNames, confid, threshold, disappeared, tracking=False):
@@ -37,69 +135,10 @@ def detection_and_tracking(vid_input, yolo_net, layerNames, confid, threshold, d
 
     # loop over each frame in video
     while True:
-        (grabbed, frame) = vs.read()
 
-        # break if using video input and no frame left to grab
-        if args["input"] is not None and frame is None:
-            print("End of Video File")
+        frame, boundingboxes = process_frame(vs, H, W, ct, yolo_net, layerNames, confid, tracking=tracking)
+        if frame is None: #error handling
             break
-
-        # resizing frames for consistency, regardless of size of input video
-        frame = imutils.resize(frame, width=1250)
-
-        # grabbing frame dimensions
-        if W is None or H is None:
-            (H, W) = frame.shape[:2]
-
-        # YOLO Detection
-        blob = cv2.dnn.blobFromImage(frame, 1 / 255.0, (416, 416), swapRB=True, crop=False)
-        yolo_net.setInput(blob)
-        layerOutputs = yolo_net.forward(layerNames)
-
-        boxes = []
-        confidences = []
-        classIDs = []
-
-        # loop over each detection
-        for output in layerOutputs:
-            for detection in output:
-                # extract the class ID and confidence of the current object detection
-                scores = detection[5:]
-                classID = np.argmax(scores)
-                confidence = scores[classID]
-
-                # filtering detections to just people with confidence above minimum confidence argument
-                if classID == 0 and confidence > confid:
-                    # scale the bounding box coordinates back relative to the size of the image
-                    box = detection[0:4] * np.array([W, H, W, H])
-                    (centerX, centerY, width, height) = box.astype("int")
-
-                    # calculate coordinates of top left point of box
-                    x = int(centerX - (width / 2))
-                    y = int(centerY - (height / 2))
-
-                    boxes.append([x, y, int(width), int(height)])
-                    confidences.append(float(confidence))
-                    classIDs.append(classID)
-
-        # apply non-maximum suppression algorithm and store final results in bounding box list
-        nms_boxes = cv2.dnn.NMSBoxes(boxes, confidences, confid, threshold)
-        boundingboxes = []
-        for i in range(len(boxes)):
-            if i in nms_boxes:
-                boundingboxes.append(boxes[i])
-
-        print("first: ", boundingboxes)
-        # call update function in centroid tracker, draw unique ID above bounding box
-
-        if tracking:
-            objects = ct.update(frame, boundingboxes)
-            # turning ordered dictionary into list of tuples
-            boundingboxes = [(ID, box) for ID, box in objects.items()]
-        else:
-            boundingboxes = [(ID, box) for ID, box in enumerate(boundingboxes)]
-
-        print("second: ", boundingboxes)
 
         w_boxes = visualize(frame, boundingboxes)
         cv2.imshow("tracking", w_boxes)
@@ -116,29 +155,20 @@ def detection_and_tracking(vid_input, yolo_net, layerNames, confid, threshold, d
     cv2.destroyAllWindows()
 
 
-# constructing argument parser and parsing arguments
-ap = argparse.ArgumentParser()
-ap.add_argument("-i", "--input", type=str,
-                help="path to input video")
-ap.add_argument("-c", "--confidence", type=float, default=0.5,
-                help="minimum probability to filter weak detections")
-ap.add_argument("-t", "--threshold", type=float, default=0.3,
-                help="threshold when applying non-maximum suppression")
-ap.add_argument("-d", "--disappeared", type=int, default=10,
-                help="number of frames that an object ID is not detected before it is de-registered")
-ap.add_argument("-tr", "--tracking", type=int, default=False,
-                help="number of frames between data output to text file")
-args = vars(ap.parse_args())
 
-# deriving the paths to the YOLO weights and model configuration
-weightsPath = os.path.sep.join(["yolo-balls", "yolov3.weights"])
-configPath = os.path.sep.join(["yolo-balls", "yolov3.cfg"])
+if __name__ == '__main__':
 
-# loading YOLO object detector
-print("Loading YOLO From Disk...")
-net = cv2.dnn.readNetFromDarknet(configPath, weightsPath)
-ln = net.getLayerNames()
-ln = [ln[i - 1] for i in net.getUnconnectedOutLayers()]
-# calling SDD function
-detection_and_tracking(args["input"], net, ln, args["confidence"], args["threshold"],
-                       args["disappeared"], args["tracking"])
+    # deriving the paths to the YOLO weights and model configuration
+    weightsPath = os.path.sep.join(["yolo-balls", "yolov3.weights"])
+    configPath = os.path.sep.join(["yolo-balls", "yolov3.cfg"])
+
+    # loading YOLO object detector
+    print("Loading YOLO From Disk...")
+    net = cv2.dnn.readNetFromDarknet(configPath, weightsPath)
+    ln = net.getLayerNames()
+    ln = [ln[i - 1] for i in net.getUnconnectedOutLayers()]
+    
+    # calling SDD function
+    args = construct_args()
+    detection_and_tracking(args["input"], net, ln, args["confidence"], args["threshold"],
+                           args["disappeared"], args["tracking"])
--- motor_controller.py
+++ motor_controller.py
@@ -1,9 +1,58 @@
 
-from PyRoboteq import roboteq_commands
+from PyRoboteq import roboteq_handler
 from PyRoboteq import roboteq_commands as cmds
 
 
+def convert_worldspace_to_motor_rot(coord):
+    '''Takes a position in the real world, and converts it to necessary
+    angles of rotation of the two motors in the corexy setup.
+    https://corexy.com/theory.html
 
+    Returns: (delta_m1, delta_m2) - rotations of each motor
+    '''
+    pulley_rad = 0 #update with actual pulley rad
+    init_coords = (0,0) #change based on actual posns read by encoders
+    deltax, deltay = [coord[i] - init_coords[i] for i in range(len(coord))]
+    
+    #use corexy principles to find change in motor linear positions.
+    #will need to adjust what "m1" and "m2" are defined as later
+    delta_m1_lin = delta_y + delta_x
+    delta_m2_lin = delta_y - delta_x
 
+    #use x = r*theta to find angular change in motor
+    delta_m1 = delta_m1_lin / pulley_rad
+    delta_m2 = delta_m2_lin / pulley_rad
 
+    return (delta_m1, delta_m2)
 
+def read_motor_controller_vals():
+    #consider making a struct that contains all the important params, and updating every n cycles
+    #by reading from motor controller
+    pass
+
+    '''
+    Motor commands of interest:
+    READ_MOTOR_AMPS = "?A" # Read current motor amperage
+    READ_BATTERY_AMPS = "?BA" # Read battery amps
+    READ_BL_MOTOR_RPM = "?BS" # Read brushless motor speed in RPM
+    READ_BLRSPEED = "?BSR" # Read brushless motor speed as 1/100 of max RPM
+    READ_ABSCNTR = "?C" # Read encoder counter absolute
+    READ_BLCNTR = "?CB" # Read absolute brushless counter
+    READ_PEAK_AMPS = "?DPA" # Read DC/Peak Amps
+    READ_DREACHED = "?DR" # Read destination reached
+    READ_FLTFLAG = "?FF" # Read fault flags
+    READ_MOTCMD = "?M" # Read motor command applied
+    READ_TEMP = "?T" # Read controller temperature
+    READ_VOLTS = "?V" # Read voltage measured
+    '''
+
+def gen_array_speed_cmds():
+    #check if position can be controller by roboteq before doing this
+    pass
+
+def set_pid_params():
+    pass
+
+def set_kinematics_params():
+    #accel, decel, max velocity, rpms at max speed
+    pass
