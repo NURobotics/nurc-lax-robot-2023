9edcea068bc4660570a51a57fe848213016cba73
seanmorton2023 <63743881+seanmorton2023@users.noreply.github.com>
2023-09-24 19:14:16 -0500

    shot display modified

--- User_Interface/Scripts/shot_history_display.py
+++ User_Interface/Scripts/shot_history_display.py
@@ -3,8 +3,6 @@ import random
 import time
 import os
 
-plt.ion()   #interactive mode
-
 # initialize arrays
 x_field_points = []
 y_field_points = []
@@ -22,9 +20,20 @@ def save_figure(figure, localpath):
     '''
     figure.savefig(os.path.join(os.path.dirname(__file__), localpath))
 
-def plot_point(x,y, filepath_in, filepath_out, extent, x_points, y_points):
+def imshow_and_save(fig, filepath_out):
+    '''Plots the image on a visible Matplotlib plot, and saves it (for use in this
+    file only - this won't be relevant to the actual lacrosse bot website
+    '''
+    plt.show()
+    save_figure(fig, filepath_out)
+    plt.pause(2)
+    plt.close()
+
+def add_point(x,y, filepath_in, extent, x_points, y_points):
     '''The base code for plot_field_point() and plot_goal_point(). We'll then
-    make those two functions a variant on this one.
+    make those two functions a variant on this one
+
+    Returns: a figure with all points, including the new point, on it
     '''
 
     plt.rcParams["figure.figsize"] = [7.0, 3.5]   # default size from copied code, manipulate for iphone14 later
@@ -52,32 +61,35 @@ def plot_point(x,y, filepath_in, filepath_out, extent, x_points, y_points):
                  markerfacecolor=colors[i], \
                  linestyle="None" \
         )
-    plt.show()
-    save_figure(fig, filepath_out)
-    plt.pause(2)
-    plt.close()
+    #plt.show()
+    #save_figure(fig, filepath_out)
+    #plt.pause(2)
+    #plt.close()
+    return fig
 
-def plot_field_point(x, y):
+def add_field_point(x, y):
     '''Plots a point with the given (x,y) coordinate on a plot.
     Any additional points passed to the function are plotted in a different color.
+
+    Returns: a figure with all points, including the new point, on it
     '''
     extent = [-14, 14, -15, 4.5]
     filepath_in = "../media/lax_field.png"
-    filepath_out = '../WebPages/field_plot.png'
     x_points = x_field_points
     y_points = y_field_points
-    plot_point(x,y, filepath_in, filepath_out, extent, x_points, y_points)
+    return plot_point(x,y, filepath_in, filepath_out, extent, x_points, y_points)
 
-def plot_goal_point(x, y):
+def add_goal_point(x, y):
     '''  Plots a point with the given (x,y) coordinate on a plot.
     Any additional points passed to the function are plotted in a different color.
+
+    Returns: a figure with all points, including the new point, on it
     '''
     extent = [-1.719, 1.719, -1.250, 1.250]
     filepath_in = "../media/lax_net_zones.png"
-    filepath_out = '../WebPages/goal_plot.png'
     x_points = x_goal_points
     y_points = y_goal_points
-    plot_point(x,y, filepath_in, filepath_out, extent, x_points, y_points)
+    return plot_point(x,y, filepath_in, filepath_out, extent, x_points, y_points)
 
 def gen_color():
     '''Generates a random color in between 0 and 2^24, converts from base-10 to hex, and 
@@ -90,15 +102,29 @@ def gen_color():
 
 if __name__ == '__main__':
 
-    #test out the plotting
-    plot_field_point(0, 0)  # plots (2,3) in blue
-    plot_goal_point(0,0)
-    plot_field_point(12, 0)
-    plot_goal_point(1,0)
-    plot_field_point(-12, 0)  # plots (4,5) in red
-    plot_goal_point(-1.375,0)
-    plot_field_point(0, -8)  # plots (6,1) in green
-    plot_goal_point(0.344, 0.25)
-    plot_field_point(0, -12)
-    plot_goal_point(-0.25, -0.75)
+    filepath_out_field = '../WebPages/field_plot.png'
+    filepath_out_goal = '../WebPages/goal_plot.png'
 
+    field_points = ( 
+        (0,0), #plots (2,3)
+        (12,0), 
+        (-12,0), #plots (4,5)
+        (0,-8), #plots (6,1)
+        (0,-12),
+    )
+
+    goal_points = (
+        (0,0),
+        (1,0),
+        (-1.375, 0),
+        (0.344, 0.25),
+        (-0.25, -0.75),
+    )
+
+    '''
+    This next step isn't expressly necessary, but I think it's a useful demo of how
+    data 
+    '''
+
+
+    plt.ion()   #interactive mode
