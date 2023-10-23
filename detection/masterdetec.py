8080896588a4c891d81aaa196447f8820ae3df20
Samuel Hodge <samuelhodge100@gmail.com>
2023-10-23 16:52:32 -0500

    Added new master file for new code updates
    Created new class for all detection related functions

---
+++ detection/masterdetec.py
@@ -0,0 +1,28 @@
+import cv2
+import numpy as np
+import time
+
+class Ball_detec():
+    def __init__(self) -> None:
+        pass
+
+    def initialize_cam():
+        # Initialize a camera video stream
+        # Eventually include multithreading
+
+        pass
+
+    def color_calibration():
+        # Complete color calibration to get the proper range of hsv values for the environment
+        pass
+    
+
+    def stereo_camera_calibration():
+        # See video : https://www.youtube.com/watch?v=uKDAVcSaNZA
+        pass
+
+    def color_mask_circle_detec():
+        # Use code from previous method : found in colordetec.py
+        pass
+
+
