cb59ce999640373efa57f6835ad1a86630da1213
Samuel Hodge <samuelhodge100@gmail.com>
2023-10-29 15:57:08 -0500

    New starting point for 2023/24 LAX CV Code
    - Added skeleton classes for detection, multicamera support, and trajectory tracking

---
+++ CV/Detection.py
@@ -0,0 +1,22 @@
+import cv2
+
+class Detection():
+    
+    # Outputs: The 2D position of the ball in the pixel space
+
+    def __init__(self) -> None:
+        pass
+
+    def initialize_cam():
+        # Initialize a camera video stream
+        # Eventually include multithreading
+
+        pass
+
+    def color_calibration():
+        # Complete color calibration for each camera to get the proper range of hsv values for the environment
+        pass
+
+    def color_mask_circle_detec():
+        # Use code from previous method : found in colordetec.py
+        pass
---
+++ CV/Stereo_Vision.py
@@ -0,0 +1,21 @@
+
+
+class Stereo_Vision():
+    
+    # Inputs : The 2D position of the ball in the pixel space
+    # Outputs: THe 3D position of the ball in the real space
+
+    def __init__(self) -> None:
+        pass
+
+    def stereo_camera_calibration():
+        # See video : https://www.youtube.com/watch?v=uKDAVcSaNZA
+        pass
+
+    def transform():
+        # Converts the inputted positions in the pixel space to the real-space
+        pass
+
+
+
+    
