9d565fc8f5597d4d7a658239c30e725ab5e2037c
Samuel Hodge <samuelhodge100@gmail.com>
2023-10-30 17:12:45 -0500

    Detection Improvements
    -incorporated camera initialization into Detection.py

--- CV/Detection.py
+++ CV/Detection.py
@@ -3,33 +3,64 @@ import cv2
 class Detection():
     
     # Outputs: The 2D position of the ball in the pixel space
+    
+    def __init__(self, N):
+        self.cap = cv2.VideoCapture(N)
+        self.frame = None
+        self.orangeUpper = None
+        self.orangeLower = None
 
-    def __init__(self) -> None:
-        pass
+        
 
-    def initialize_cam(N):
-        # N specifies the camera number 0 for mac webcam, typically 1 for mac webcam
-        # Initialize a camera video stream
+    def get_frame(self):
+        # Outputs the frame from the cap object
         # Eventually include multithreading
-        cap = cv2.VideoCapture(N)
+        ret,frame = self.cap.read()
+        if ret:
+            self.frame = frame
+        else:
+            self.frame = None
 
-        return cap
 
-    def color_calibration(cap):
+
+
+
+    def color_calibration(self):
         # Complete color calibration for each camera to get the proper range of hsv values for the environment
-        while(1):
-            ret, frame = cap.read()
-            if not ret :
-                print("Error: Could not read frame")
-                break
 
-            # Create a window to display the video stream
-            cv2.namedWindow('Video Stream')
-            cv2.setMouseCallback('Video Stream', get_hsv_range)
+        #self.orangeUpper = ...
+        #self.orangeLower = ...
+        pass
             
 
         
 
-    def color_mask_circle_detec():
+    def color_mask_circle_detec(self):
+        # Inputs a frame from self
+        # Convert rgb to hsv space
+
+
+        # Outputs coordinates of the ball with radius in the form:  (x,y,r) 
         # Use code from previous method : found in colordetec.py
+
         pass
+
+    
+    def release_camera(self):
+        self.cap.release()
+        
+
+
+# Test code 
+Cam1 = Detection(0)
+cv2.namedWindow('Video Stream')
+while(1):
+    Cam1.get_frame()
+    cv2.imshow("Video Stream", Cam1.frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        Cam1.release_camera()
+        break
+cv2.destroyAllWindows()
+
+
