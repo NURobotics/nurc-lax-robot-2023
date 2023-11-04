9bc5728b98eee2082303d3f24b073beedc7ebb6d
Vincent Rimparsurat <vrim@u.northwestern.edu>
2023-11-04 00:26:49 -0500

    Update color_finder.py to Python 3

---
+++ detection/color_finder.py
@@ -0,0 +1,54 @@
+"""
+File name :
+    color_finder.py
+Purpose :
+    Test different color masks on webcam input
+Last updated :
+    2023-11-04 by Vincent Rimparsurat (vrim@u.northwestern.edu)
+"""
+import cv2 as cv
+import numpy as np
+
+def nothing(x):
+    pass
+
+# Create a window named trackbars.
+cv.namedWindow("Trackbars")
+
+# Now create 6 trackbars that will control the lower and upper range of 
+# H,S and V channels. The Arguments are like this: Name of trackbar, 
+# window name, range,callback function. For Hue the range is 0-179 and
+# for S,V its 0-255.
+cv.createTrackbar("L - H", "Trackbars", 0, 179, nothing)
+cv.createTrackbar("L - S", "Trackbars", 0, 255, nothing)
+cv.createTrackbar("L - V", "Trackbars", 0, 255, nothing)
+cv.createTrackbar("U - H", "Trackbars", 179, 179, nothing)
+cv.createTrackbar("U - S", "Trackbars", 255, 255, nothing)
+cv.createTrackbar("U - V", "Trackbars", 255, 255, nothing)
+
+cap = cv.VideoCapture(0)
+
+if not cap.isOpened():
+    print("Cannot open camera")
+    exit()
+
+while True:
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+
+    # if frame is read correctly ret is True
+    if not ret:
+        print("Can't receive frame (stream end?). Exiting ...")
+        break
+
+    # Our operations on the frame come here
+    gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+
+    # Display the resulting frame
+    cv.imshow("frame", gray)
+    if cv.waitKey(1) == ord("q"):
+        break
+
+# When everything done, release the capture
+cap.release()
+cv.destroyAllWindows()
