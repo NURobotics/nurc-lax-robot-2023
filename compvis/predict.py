058c62e6ee210ed625804001e5058cdf3b2aca62
Vincent Rimparsurat <rimvincent@gmail.com>
2024-01-11 17:45:22 -0600

    add code for triangulation and prediction

---
+++ compvis/predict.py
@@ -0,0 +1,105 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+class RLS:
+    """Implementation from https://github.com/craig-m-k/Recursive-least-squares"""
+
+    def __init__(self, num_vars, lam, delta=1):
+        """
+        num_vars: number of variables including constant
+        lam: forgetting factor, usually very close to 1.
+        """
+        self.num_vars = num_vars
+
+        # delta controls the initial state.
+        self.A = delta * np.matrix(np.identity(self.num_vars))
+        self.w = np.matrix(np.zeros(self.num_vars))
+        self.w = self.w.reshape(self.w.shape[1], 1)
+
+        # Variables needed for add_obs
+        self.lam_inv = lam ** (-1)
+        self.sqrt_lam_inv = np.sqrt(self.lam_inv)
+
+        # A priori error
+        self.a_priori_error = 0
+
+        # Count of number of observations added
+        self.num_obs = 0
+
+    def add_obs(self, x, t):
+        """
+        Add the observation x with label t.
+        x is a column vector as a numpy matrix
+        t is a real scalar
+        """
+        z = self.lam_inv * self.A * x
+        alpha = (1 + (x.T @ z).item()) ** (-1)
+        self.a_priori_error = t - (self.w.T @ x).item()
+        self.w = self.w + (t - alpha * (x.T @ (self.w + t * z)).item()) * z
+        self.A -= alpha * z * z.T
+        self.num_obs += 1
+
+    def fit(self, X, y):
+        """
+        Fit a model to X,y.
+        X and y are numpy arrays.
+        Individual observations in X should have a prepended 1 for constant coefficient.
+        """
+        for i in range(len(X)):
+            x = np.transpose(np.matrix(X[i]))
+            self.add_obs(x, y[i])
+
+    def get_error(self):
+        """
+        Finds the a priori (instantaneous) error.
+        Does not calculate the cumulative effect
+        of round-off errors.
+        """
+        return self.a_priori_error
+
+    def predict(self, x):
+        """
+        Predict the value of observation x. x should be a numpy matrix (col vector)
+        """
+        return float(self.w.T * x)
+
+def regression_test():
+    # Predicting a quadratic function
+    test_size = 1000
+    # Test function
+    f = lambda x: 0.2 * x**2 - 3.8 * x - 5.1
+    # Gaussian noise to be added to the quadratic signal
+    noise = np.random.randn(test_size)
+    # You can play around with other noise (like sinusoidal)
+    # noise = [np.sin(2*np.pi*i/13) for i in range(test_size)]
+    y = np.array([f(i) for i in range(test_size)])
+    noisy_y = y + noise
+    lam = 0.98
+    LS = RLS(3, lam, 1)
+    # Not using the RLS.fit function because I want to remember all the predicted values
+    pred_x = []
+    pred_y = []
+    for i in range(test_size):
+        x = np.matrix(np.zeros((1, 3)))
+        x[0, 0] = 1
+        x[0, 1] = i
+        x[0, 2] = i**2
+        pred_x.append(i)
+        pred_y.append((x @ LS.w).item())
+        LS.add_obs(x.T, noisy_y[i])
+
+    print("Predicted equation in the form Ax^2 + Bx + C")
+    print(f"A = {LS.w[2, 0]} | Real value = 0.2")
+    print(f"B = {LS.w[1, 0]} | Real value = -3.8")
+    print(f"C = {LS.w[0, 0]} | Real value = -5.1")
+    # plot the predicted values against the non-noisy output
+    plt.plot(pred_x, y - pred_y)
+    plt.title("Error as more points are added")
+    plt.show()
+
+if __name__ == "__main__":
+    regression_test()
+    # To deal with the output being dimensions in R^3, it seems sufficient to
+    # deal with each dimension separately
+    # (See https://math.stackexchange.com/q/2688132)
---
+++ compvis/triangulation.py
@@ -0,0 +1,175 @@
+import numpy as np
+
+
+class LSLocalizer:
+    """Takes multiple rays to predict a position using a least squares method.
+
+    Stores the space transforms of each camera in the form of SE(3) matrices to
+    unify objects detections from each viewpoint. The convention for coordinates
+    is that X is to the right of the camera, Y is in the direction of the
+    camera, and Z is up from the camera.
+
+    Rotations are represented as a pair of angles theta and phi rotating a unit
+    vector in the Y direction about the Z and X axes respectively. A positive
+    theta rotates the vector towards positive X and a positive phi rotates
+    the vector towards positive Z.
+    """
+
+    def __init__(self, camera_transforms):
+        self.camera_transforms = np.array(camera_transforms)
+
+    def predict(self, angle_pair_array, weights):
+        ray_directions = self.angles_to_rays(angle_pair_array)
+        ray_points = self.camera_transforms[:, :3, 3]
+        # print(f"{ray_points = }")
+        return self.find_nearest_point(ray_points, ray_directions, weights)
+
+    def angles_to_rays(self, ray_angles):
+        transformed_rays = []
+        for i in range(len(ray_angles)):
+            theta, phi = ray_angles[i]
+            transform = self.camera_transforms[i]
+            transform_origin = transform[:, 3]
+
+            ray = [
+                np.cos(phi) * np.sin(theta),
+                np.cos(phi) * np.cos(theta),
+                np.sin(phi),
+                1,
+            ]
+            ray = transform @ ray
+            ray -= transform_origin
+            transformed_rays.append(ray[:3])
+
+        return np.array(transformed_rays)
+
+    def find_nearest_point(self, ray_points, ray_directions, weights=None):
+        """Implements the method found at https://stackoverflow.com/a/48201730.
+
+        Finds the point that minimizes the least squared distance to each of the
+        rays. Utilizes the magic number 3 in places as it only works in R^3
+        (it may work in higher dimension but that proof uses cross products).
+        """
+        m = np.zeros((3, 3))
+        b = np.zeros(3)
+
+        n = len(ray_points)
+
+        if not len(ray_directions) == n or not len(weights) == n:
+            print("Size of ray_points and ray_directions do not match")
+            return np.zeros(3)
+
+        if weights is None:
+            weights = np.ones(n)
+
+        direction_magnitudes = np.linalg.norm(ray_directions, axis=1)
+        ray_directions = weights[:, None] * (
+            ray_directions / direction_magnitudes[:, None]
+        )
+
+        for i in range(n):
+            dd = np.dot(ray_directions[i], ray_directions[i])
+            da = np.dot(ray_directions[i], ray_points[i])
+            for j in range(3):
+                for k in range(3):
+                    m[j, k] += ray_directions[i][j] * ray_directions[i][k]
+                m[j, j] -= dd
+                b[j] += ray_directions[i, j] * da - ray_points[i][j] * dd
+
+        return np.linalg.solve(m, b)
+
+
+# Not implemented correctly - needs 3 angles not 2
+# def angles_to_transform(angles):
+#     theta, phi = angles
+#     transform = np.array(
+#         [
+#             [
+#                 np.cos(phi) * np.cos(theta),
+#                 np.cos(phi) * np.sin(theta),
+#                 np.sin(phi) * -np.sin(theta),
+#                 0,
+#             ],
+#             [
+#                 np.cos(phi) * -np.sin(theta),
+#                 np.cos(phi) * np.cos(theta),
+#                 np.sin(phi) * -np.cos(theta),
+#                 0,
+#             ],
+#             [
+#                 np.sin(phi),
+#                 np.sin(phi),
+#                 np.cos(phi),
+#                 0,
+#             ],
+#             [
+#                 0,
+#                 0,
+#                 0,
+#                 1,
+#             ],
+#         ]
+#     )
+#     return transform
+
+
+if __name__ == "__main__":
+    np.set_printoptions(formatter={"float": "{: 0.4f}".format})
+
+    # first camera at origin
+    T_cam1 = np.array(
+        [
+            [1, 0, 0, 0],
+            [0, 1, 0, 0],
+            [0, 0, 1, 0],
+            [0, 0, 0, 1],
+        ]
+    )
+
+    # second camera rotated pi/2 about Z at (1, 1, 0)
+    T_cam2 = np.array(
+        [
+            [0, -1, 0, 1],
+            [1, 0, 0, 1],
+            [0, 0, 1, 0],
+            [0, 0, 0, 1],
+        ]
+    )
+    camera_transforms = [T_cam1, T_cam2]
+
+    lsl = LSLocalizer(camera_transforms)
+
+    # test find_nearest_point
+    ray_points = np.array(
+        [
+            [1, 0, 0],
+            [0, 1, 0],
+            [0, 0, 0],
+            [0, 0, 0],
+        ]
+    )
+    ray_directions = np.array(
+        [
+            [0, 1, 0],
+            [1, 0, 0],
+            [0, 1, 0],
+            [1, 0, 0],
+        ]
+    )
+
+    weights = np.array([1, 1, 1, 1])
+
+    nearest_point = lsl.find_nearest_point(ray_points, ray_directions, weights)
+    print(f"{nearest_point = }")
+
+    # test angles_to_ray
+    ray_1_angles = (0, 0)
+    ray_2_angles = (np.pi / 4, 0)
+    ray_angles = [ray_1_angles, ray_2_angles]
+
+    transformed_rays = lsl.angles_to_rays(ray_angles)
+    print(f"transformed_rays = \n{transformed_rays}")
+
+    # test predict
+    predicted_point = lsl.predict(ray_angles, np.array([1, 1]))
+    print(f"{predicted_point = }")
